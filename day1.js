15637a2d230b79c273a67069f8cefe24f4793863
Joraeuw <joraeuw@gmail.com>
2021-12-01 14:32:50 +0200

    Day 1 👨‍💻

---
+++ day1.js
@@ -0,0 +1,9 @@
+const input = require("./input").module.inputNums;
+
+let result = 0;
+
+for (let index = 2; index < input.length - 1; index++) {
+  if (input[index + 1] > input[index - 2]) result++;
+}
+
+console.log(result);
